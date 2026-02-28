ee5b056b73ce50cb72cb16c7091baea134e34124
MaminProgrammist <kit04048@gmail.com>
2026-03-01 01:23:30 +0300

    upd new main

--- src/main.cpp
+++ src/main.cpp
@@ -1,23 +1,37 @@
 #include <SDL3/SDL.h>
+#include <vector>
+
+const int WIDTH = 800;
+const int HEIGHT = 600;
 
 int main(int argc, char* argv[]) {
     SDL_Init(SDL_INIT_VIDEO);
-
-    SDL_Window* window = SDL_CreateWindow("Software Renderer", 800, 600, 0);
+    SDL_Window* window = SDL_CreateWindow("Software Renderer", WIDTH, HEIGHT, 0);
     SDL_Renderer* renderer = SDL_CreateRenderer(window, nullptr);
+    SDL_Texture* texture = SDL_CreateTexture(renderer,
+        SDL_PIXELFORMAT_RGBA8888,
+        SDL_TEXTUREACCESS_STREAMING,
+        WIDTH, HEIGHT);
+
+    // Наш framebuffer - просто массив пикселей
+    std::vector<uint32_t> framebuffer(WIDTH * HEIGHT);
 
     bool running = true;
     while (running) {
         SDL_Event event;
-        while (SDL_PollEvent(&event)) {
+        while (SDL_PollEvent(&event))
             if (event.type == SDL_EVENT_QUIT) running = false;
-        }
 
-        SDL_SetRenderDrawColor(renderer, 100, 149, 237, 255);
-        SDL_RenderClear(renderer);
+        // Заливаем все пиксели красным цветом
+        for (auto& pixel : framebuffer)
+            pixel = 0xFF0000FF;
+
+        SDL_UpdateTexture(texture, nullptr, framebuffer.data(), WIDTH * sizeof(uint32_t));
+        SDL_RenderTexture(renderer, texture, nullptr, nullptr);
         SDL_RenderPresent(renderer);
     }
 
+    SDL_DestroyTexture(texture);
     SDL_DestroyRenderer(renderer);
     SDL_DestroyWindow(window);
     SDL_Quit();
