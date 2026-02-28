f3c1d92207dea4fbab7637b4d09a053ea8d9bd93
MaminProgrammist <kit04048@gmail.com>
2026-02-28 16:46:45 +0300

    final project init

--- external/SDL3/cmake/SDL3ConfigVersion.cmake
+++ external/SDL3/cmake/SDL3ConfigVersion.cmake
@@ -1,38 +1,19 @@
 # SDL3 CMake version configuration file:
-# This file is meant to be placed in a cmake subfolder of SDL3-devel-3.4.2-VC.zip
+# This file is meant to be placed in a cmake subfolder of SDL3-devel-3.x.y-mingw
 
-set(PACKAGE_VERSION "3.4.2")
-
-if(PACKAGE_FIND_VERSION_RANGE)
-    # Package version must be in the requested version range
-    if ((PACKAGE_FIND_VERSION_RANGE_MIN STREQUAL "INCLUDE" AND PACKAGE_VERSION VERSION_LESS PACKAGE_FIND_VERSION_MIN)
-        OR ((PACKAGE_FIND_VERSION_RANGE_MAX STREQUAL "INCLUDE" AND PACKAGE_VERSION VERSION_GREATER PACKAGE_FIND_VERSION_MAX)
-        OR (PACKAGE_FIND_VERSION_RANGE_MAX STREQUAL "EXCLUDE" AND PACKAGE_VERSION VERSION_GREATER_EQUAL PACKAGE_FIND_VERSION_MAX)))
-        set(PACKAGE_VERSION_COMPATIBLE FALSE)
-    else()
-        set(PACKAGE_VERSION_COMPATIBLE TRUE)
-    endif()
+if(CMAKE_SIZEOF_VOID_P EQUAL 4)
+    set(sdl3_config_path "${CMAKE_CURRENT_LIST_DIR}/../i686-w64-mingw32/lib/cmake/SDL3/SDL3ConfigVersion.cmake")
+elseif(CMAKE_SIZEOF_VOID_P EQUAL 8)
+    set(sdl3_config_path "${CMAKE_CURRENT_LIST_DIR}/../x86_64-w64-mingw32/lib/cmake/SDL3/SDL3ConfigVersion.cmake")
 else()
-    if(PACKAGE_VERSION VERSION_LESS PACKAGE_FIND_VERSION)
-        set(PACKAGE_VERSION_COMPATIBLE FALSE)
-    else()
-        set(PACKAGE_VERSION_COMPATIBLE TRUE)
-        if(PACKAGE_FIND_VERSION STREQUAL PACKAGE_VERSION)
-            set(PACKAGE_VERSION_EXACT TRUE)
-        endif()
-    endif()
-endif()
-
-# if the using project doesn't have CMAKE_SIZEOF_VOID_P set, fail.
-if("${CMAKE_SIZEOF_VOID_P}" STREQUAL "")
     set(PACKAGE_VERSION_UNSUITABLE TRUE)
+    return()
 endif()
 
-include("${CMAKE_CURRENT_LIST_DIR}/sdlcpu.cmake")
-SDL_DetectTargetCPUArchitectures(_detected_archs)
-
-# check that the installed version has a compatible architecture as the one which is currently searching:
-if(NOT(SDL_CPU_X86 OR SDL_CPU_X64 OR SDL_CPU_ARM64 OR SDL_CPU_ARM64EC))
-    set(PACKAGE_VERSION "${PACKAGE_VERSION} (X86,X64,ARM64)")
+if(NOT EXISTS "${sdl3_config_path}")
+    message(WARNING "${sdl3_config_path} does not exist: MinGW development package is corrupted")
     set(PACKAGE_VERSION_UNSUITABLE TRUE)
+    return()
 endif()
+
+include("${sdl3_config_path}")
