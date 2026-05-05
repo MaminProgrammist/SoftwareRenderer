a4bf46c536db43b8ef425938f566607a78d7815e
MaminProgrammist <kit04048@gmail.com>
2026-05-05 15:56:26 +0300

    basic render upd

--- src/main.cpp
+++ src/main.cpp
@@ -5,12 +5,12 @@
 #include "math.h"
 #include <limits>
 
-const int WIDTH = 600;
-const int HEIGHT = 600;
-const float D = 200;
+const int WIDTH = 800;
+const int HEIGHT = 800;
+const float FOV = 60;
+const float D = (WIDTH / 2.0f) / std::tan(FOV * M_PI / 360.0f);
 
-struct Cube
-{
+struct Cube {
     float side;
     Vec4 center;
     Vec4 vertices[8];
@@ -30,13 +30,12 @@ struct Cube
         2, 3, 4
     };
 
-    Cube(float side, Vec4 center)
-    {
+    Cube(float side, Vec4 center) {
         this->side = side;
         this->center = center;
 
         vertices[0] = Vec4();
-        vertices[1] = Vec4(side, 0 ,0);
+        vertices[1] = Vec4(side, 0, 0);
         vertices[2] = Vec4(side, side, 0);
         vertices[3] = Vec4(0, side, 0);
         vertices[4] = Vec4(0, side, side);
@@ -44,41 +43,42 @@ struct Cube
         vertices[6] = Vec4(side, 0, side);
         vertices[7] = Vec4(side, side, side);
 
-        Vec4 shift_v = Vec4::invert(center);
-        Mat4 shift_m = Mat4::mat4_translation(shift_v.x, shift_v.y, shift_v.z);
+        Vec4 shift_vec = center;
+        shift_vec.add(Vec4(-side/2.0f, -side/2.0f, -side/2.0f, 0));
+        Mat4 shift_mat = Mat4::mat4_translation(shift_vec.x, shift_vec.y, shift_vec.z);
 
         for (int i = 0; i < 8; i++)
-            vertices[i] = Mat4::mat4_multiply_vec4(shift_m, vertices[i]);
+            vertices[i] = Mat4::mat4_multiply_vec4(shift_mat, vertices[i]);
     }
 };
 
-Vec4 project(Vec4& p, float d)
-{
+Vec4 project(const Vec4 &p, float d) {
     Vec4 result;
 
-    result.x = (p.x * d) / p.z + WIDTH / 2.0f;
-    result.y = (p.y * d) / p.z + HEIGHT / 2.0f;
+    result.x = (p.x * d) / p.z;
+    result.y = (p.y * d) / p.z;
     result.z = p.z;
     result.w = 1;
+
+    result.x += WIDTH / 2.0f;
+    result.y += HEIGHT / 2.0f;
 
     return result;
 }
 
 // принимает z трёх вершин, возвращает яркость от 0.0 до 1.0
-float calc_brightness(float z0, float z1, float z2, float z_min, float z_max)
-{
+float check_depth_brightness(float z0, float z1, float z2, float z_min, float z_max) {
     float br = (z0 + z1 + z2) / 3.0f;
     br = (z_max - br) / (z_max - z_min);
     return br;
 }
 
 // принимает базовый цвет и яркость, возвращает затемнённый цвет
-uint32_t apply_brightness(uint32_t color, float brightness)
-{
+uint32_t apply_brightness(uint32_t color, float brightness) {
     // взяли каждый канал
-    uint32_t r = (color>>24) & 0xFF;
-    uint32_t g = (color>>16) & 0xFF;
-    uint32_t b = (color>>8) & 0xFF;
+    uint32_t r = (color >> 24) & 0xFF;
+    uint32_t g = (color >> 16) & 0xFF;
+    uint32_t b = (color >> 8) & 0xFF;
 
     // применяем яркость и каст обратно
     r = static_cast<uint32_t>(brightness * r) << 24;
@@ -88,24 +88,21 @@ uint32_t apply_brightness(uint32_t color, float brightness)
     return r | g | b | 0xFF;
 }
 
-void set_pixel(std::vector<uint32_t>& framebuffer, int x, int y, uint32_t color)
-{
+void set_pixel(std::vector<uint32_t> &framebuffer, int x, int y, uint32_t color) {
     if (x < 0 || x >= WIDTH || y < 0 || y >= HEIGHT)
         return;
 
     framebuffer[y * WIDTH + x] = color;
 }
 
-void draw_line(std::vector<uint32_t>& framebuffer, int x0, int y0, int x1, int y1, uint32_t color)
-{
+void draw_line(std::vector<uint32_t> &framebuffer, int x0, int y0, int x1, int y1, uint32_t color) {
     int dx = abs(x1 - x0);
     int dy = abs(y1 - y0);
     int sx = (x0 < x1) ? 1 : -1; // направление по X
     int sy = (y0 < y1) ? 1 : -1; // направление по Y
     int err = dx - dy;
 
-    while (true)
-    {
+    while (true) {
         set_pixel(framebuffer, x0, y0, color);
 
         if (x0 == x1 && y0 == y1)
@@ -113,37 +110,31 @@ void draw_line(std::vector<uint32_t>& framebuffer, int x0, int y0, int x1, int y
 
         int e2 = 2 * err;
 
-        if (e2 > -dy)
-        {
+        if (e2 > -dy) {
             err -= dy;
             x0 += sx;
         }
 
-        if (e2 < dx)
-        {
+        if (e2 < dx) {
             err += dx;
             y0 += sy;
         }
     }
 }
 
-int check_side(int xPoint, int yPoint, int x0, int y0, int x1, int y1)
-{
+int check_side(int xPoint, int yPoint, int x0, int y0, int x1, int y1) {
     int side = (x1 - x0) * (yPoint - y0) - (y1 - y0) * (xPoint - x0);
     return side >= 0 ? 1 : -1;
 }
 
-void fill_triangle(std::vector<uint32_t>& framebuffer, int x0, int y0, int x1, int y1, int x2, int y2, uint32_t color)
-{
+void fill_triangle(std::vector<uint32_t> &framebuffer, int x0, int y0, int x1, int y1, int x2, int y2, uint32_t color) {
     int xmin = std::min(std::min(x0, x1), x2);
     int ymin = std::min(std::min(y0, y1), y2);
     int xmax = std::max(std::max(x0, x1), x2);
     int ymax = std::max(std::max(y0, y1), y2);
 
-    for (int x = xmin; x < xmax; x++)
-    {
-        for (int y = ymin; y < ymax; y++)
-        {
+    for (int x = xmin; x < xmax; x++) {
+        for (int y = ymin; y < ymax; y++) {
             int s0 = check_side(x, y, x0, y0, x1, y1);
             int s1 = check_side(x, y, x1, y1, x2, y2);
             int s2 = check_side(x, y, x2, y2, x0, y0);
@@ -154,47 +145,40 @@ void fill_triangle(std::vector<uint32_t>& framebuffer, int x0, int y0, int x1, i
     }
 }
 
-void draw_triangle(std::vector<uint32_t>& framebuffer, const Vec4& p0, const Vec4& p1, const Vec4& p2, uint32_t color)
-{
+void draw_triangle(std::vector<uint32_t> &framebuffer, const Vec4 &p0, const Vec4 &p1, const Vec4 &p2, uint32_t color) {
     fill_triangle(framebuffer, p0.x, p0.y, p1.x, p1.y, p2.x, p2.y, color);
 }
 
 void draw_wired_triangle(
-    std::vector<uint32_t>& framebuffer,
-    const Vec4& p0, const Vec4& p1, const Vec4& p2,
-    uint32_t color)
-{
+    std::vector<uint32_t> &framebuffer,
+    const Vec4 &p0, const Vec4 &p1, const Vec4 &p2,
+    uint32_t color) {
     draw_line(framebuffer, p0.x, p0.y, p1.x, p1.y, color);
     draw_line(framebuffer, p1.x, p1.y, p2.x, p2.y, color);
     draw_line(framebuffer, p2.x, p2.y, p0.x, p0.y, color);
 }
 
-void draw_wired_cube(std::vector<uint32_t>& framebuffer, const Cube& cube, uint32_t color, bool depthShade = false)
-{
+void draw_wired_cube(std::vector<uint32_t> &framebuffer, const Cube &cube, uint32_t color, bool depthShade = false) {
     float z_min = std::numeric_limits<float>::max();
     float z_max = std::numeric_limits<float>::lowest();
 
-    for (int i = 0 ; i < 8 ; i++)
-    {
+    for (int i = 0; i < 8; i++) {
         z_min = std::min(z_min, cube.vertices[i].z);
         z_max = std::max(z_max, cube.vertices[i].z);
     }
 
-    for (int i = 0 ; i < 12 ; i++)
-    {
+    for (int i = 0; i < 12; i++) {
         Vec4 indexes[3];
 
-        for (int j = 0 ; j < 3 ; j++)
-        {
-            indexes[j] = cube.vertices[cube.order[i*3 + j]];
+        for (int j = 0; j < 3; j++) {
+            indexes[j] = cube.vertices[cube.order[i * 3 + j]];
             indexes[j] = project(indexes[j], D);
         }
 
         uint32_t filtered_color = color;
 
-        if (depthShade)
-        {
-            float brightness = calc_brightness(indexes[0].z, indexes[1].z, indexes[2].z, z_min, z_max);
+        if (depthShade) {
+            float brightness = check_depth_brightness(indexes[0].z, indexes[1].z, indexes[2].z, z_min, z_max);
             filtered_color = apply_brightness(color, brightness);
             //filtered_color = 0x0000FFFF;
         }
@@ -203,44 +187,70 @@ void draw_wired_cube(std::vector<uint32_t>& framebuffer, const Cube& cube, uint3
     }
 }
 
-int main(int argc, char* argv[])
-{
-    SDL_Init(SDL_INIT_VIDEO);
-    SDL_Window* window = SDL_CreateWindow("Software Renderer", WIDTH, HEIGHT, 0);
-    SDL_Renderer* renderer = SDL_CreateRenderer(window, nullptr);
+void draw_dot(std::vector<uint32_t> &framebuffer, Vec4 p, int size, uint32_t color, bool depthShade = false) {
+    int xMin = p.x - size;
+    int yMin = p.y - size;
+    int xMax = p.x + size;
+    int yMax = p.y + size;
 
-    SDL_Texture* texture = SDL_CreateTexture(renderer,SDL_PIXELFORMAT_RGBA8888,SDL_TEXTUREACCESS_STREAMING,WIDTH, HEIGHT);
+    for (int i = xMin; i < xMax; i++) {
+        for (int j = yMin; j < yMax; j++) {
+            set_pixel(framebuffer, i, j, color);
+        }
+    }
+}
+
+void draw_dot_cube(std::vector<uint32_t> &framebuffer, const Cube &cube, uint32_t color, bool depthShade = false) {
+    uint32_t colors[8] = {
+        0x0000FFFF, //
+        0x0000FFFF, //
+        0x00FFFFFF, //
+        0x00FFFFFF, //
+        0xFF00FFFF, //
+        0xFF00FFFF, //
+        0xFFFFFFFF, //
+        0xFFFFFFFF, //
+    };
+
+    for (int i = 0; i < 8; i++) {
+        Vec4 p = project(cube.vertices[i], D);
+        draw_dot(framebuffer, p, 2, colors[i]);
+    }
+}
+
+void rotate_cube_y(Cube &cube, float angle) {
+    const Mat4 rot = Mat4::mat4_rotation_y(angle);
+
+    for (int i = 0; i < 8; i++) {
+        cube.vertices[i] = Mat4::mat4_multiply_vec4(rot, cube.vertices[i]);
+    }
+}
+
+int main(int argc, char *argv[]) {
+    SDL_Init(SDL_INIT_VIDEO);
+    SDL_Window *window = SDL_CreateWindow("Software Renderer", WIDTH, HEIGHT, 0);
+    SDL_Renderer *renderer = SDL_CreateRenderer(window, nullptr);
+    SDL_Texture *texture = SDL_CreateTexture(renderer, SDL_PIXELFORMAT_RGBA8888, SDL_TEXTUREACCESS_STREAMING, WIDTH, HEIGHT);
 
     // Наш framebuffer - просто массив пикселей
     std::vector<uint32_t> framebuffer(WIDTH * HEIGHT);
 
-    // вершины треугольника
-    Vec4 A = Vec4(300, 100, 0, 1);
-    Vec4 B = Vec4(500, 500, 0, 1);
-    Vec4 C = Vec4(100, 500, 0, 1);
-    Mat4 T = Mat4::mat4_translation(100, 0, 0);
-
-    Cube cube(200, Vec4(0, 0, 350));
-
+    Cube cube(1, Vec4(0, 0, 5));
     uint32_t color = 0xFFFFFFFF;
 
+    float angle = 0.01;
     bool running = true;
-    bool swap = false;
+    bool swap = true;
 
-    while (running)
-    {
+    while (running) {
         SDL_Event event;
 
-        while (SDL_PollEvent(&event))
-        {
-            if (event.type == SDL_EVENT_KEY_DOWN)
-            {
-                color = 0xFF0000FF;
-                // A = Mat4::mat4_multiply_vec4(T, A);
-                // B = Mat4::mat4_multiply_vec4(T, B);
-                // C = Mat4::mat4_multiply_vec4(T, C);
-                swap = true;
-            }
+        while (SDL_PollEvent(&event)) {
+            if (event.key.scancode == SDL_SCANCODE_LEFT)
+                rotate_cube_y(cube, -1 * angle);
+
+            if (event.key.scancode == SDL_SCANCODE_RIGHT)
+                rotate_cube_y(cube, angle);
 
             if (event.type == SDL_EVENT_QUIT)
                 running = false;
@@ -249,10 +259,9 @@ int main(int argc, char* argv[])
         for (int i = 0; i < WIDTH * HEIGHT; i++)
             framebuffer[i] = 0x000000FF;
 
-        if (swap)
-            draw_wired_cube(framebuffer, cube, color, true);
-        else
-            draw_wired_triangle(framebuffer, A, B, C, color);
+        //draw_wired_cube(framebuffer, cube, color, true);
+        //draw_dot(framebuffer, Vec4(WIDTH / 2, HEIGHT / 2, 1, 1), 10, color);
+        draw_dot_cube(framebuffer, cube, color);
 
         SDL_UpdateTexture(texture, nullptr, framebuffer.data(), WIDTH * sizeof(uint32_t));
         SDL_RenderTexture(renderer, texture, nullptr, nullptr);
--- src/math.h
+++ src/math.h
@@ -3,154 +3,161 @@
 
 #include <cmath>
 
-struct Vec3
-{
+struct Vec3 {
     float x, y, z;
 
-    Vec3 (float x, float y, float z)
-    {
+    Vec3(float x, float y, float z) {
         this->x = x;
         this->y = y;
         this->z = z;
     }
 
-    static Vec3 sum(const Vec3& a, const Vec3& b)
-    {
+    static Vec3 sum(const Vec3 &a, const Vec3 &b) {
         Vec3 c(a.x + b.x, a.y + b.y, a.z + b.z);
         return c;
     }
 
-    static float dot(const Vec3& a, const Vec3& b)
-    {
+    static float dot(const Vec3 &a, const Vec3 &b) {
         return a.x * b.x + a.y * b.y + a.z * b.z;
     }
 
-    static Vec3 cross(const Vec3& a, const Vec3& b)
-    {
-        Vec3 c(a.y*b.z - a.z*b.y, a.z*b.x - a.x*b.z, a.x*b.y - a.y*b.x);
+    static Vec3 cross(const Vec3 &a, const Vec3 &b) {
+        Vec3 c(a.y * b.z - a.z * b.y, a.z * b.x - a.x * b.z, a.x * b.y - a.y * b.x);
         return c;
     }
 
-    void add(const Vec3& a)
-    {
-        x += a.x;
-        y += a.y;
-        z += a.z;
+    void add(const Vec3 &v) {
+        x += v.x;
+        y += v.y;
+        z += v.z;
     }
 
-    void sub(const Vec3& a)
-    {
-        x -= a.x;
-        y -= a.y;
-        z -= a.z;
+    void sub(const Vec3 &v) {
+        x -= v.x;
+        y -= v.y;
+        z -= v.z;
     }
 
-    void scale(float s)
-    {
+    void scale(float s) {
         x *= s;
         y *= s;
         z *= s;
     }
 
-    void invert()
-    {
+    void invert() {
         scale(-1);
     }
 
-    float length()
-    {
+    float length() {
         return std::sqrt(x * x + y * y + z * z);
     }
 
-    void normalize()
-    {
+    void normalize() {
         float l = length();
         scale(1.0f / l);
     }
 };
 
-struct Vec4
-{
+struct Vec4 {
     float x, y, z, w;
 
-    Vec4()
-    {
+    Vec4() {
         this->x = 0;
         this->y = 0;
         this->z = 0;
         this->w = 1;
     }
 
-    Vec4(float x, float y, float z)
-    {
+    Vec4(float x, float y, float z) {
         this->x = x;
         this->y = y;
         this->z = z;
         w = 1;
     }
 
-    Vec4(float x, float y, float z, float w)
-    {
+    Vec4(float x, float y, float z, float w) {
         this->x = x;
         this->y = y;
         this->z = z;
         this->w = w;
     }
 
-    void scale(float s)
-    {
+    void scale(float s) {
         x *= s;
         y *= s;
         z *= s;
     }
 
-    static Vec4 invert(const Vec4& v)
-    {
+    void add(const Vec4& v) {
+        x += v.x;
+        y += v.y;
+        z += v.z;
+        w += v.w;
+    }
+
+    static Vec4 invert(const Vec4 &v) {
         Vec4 inv_v = v;
         inv_v.scale(-1);
         return inv_v;
     }
 };
 
-struct Mat4
-{
+struct Mat4 {
     float m[16];
 
-    Mat4()
-    {
+    Mat4() {
         for (int i = 0; i < 16; i++)
             m[i] = 0;
     }
 
-    static Mat4 identity()
-    {
+    static Mat4 identity() {
         Mat4 e;
 
         for (int i = 0; i < 4; i++)
-            e.m[i*4 + i] = 1;
+            e.m[i * 4 + i] = 1;
 
         return e;
     }
 
-    static Vec4 mat4_multiply_vec4 (const Mat4& mat, const Vec4& vec)
-    {
+    static Vec4 mat4_multiply_vec4(const Mat4 &mat, const Vec4 &vec) {
         Vec4 result = Vec4();
 
-        result.x =  mat.m[0] * vec.x + mat.m[1] * vec.y + mat.m[2] * vec.z + mat.m[3] * vec.w;
-        result.y =  mat.m[4] * vec.x + mat.m[5] * vec.y + mat.m[6] * vec.z + mat.m[7] * vec.w;
-        result.z =  mat.m[8] * vec.x + mat.m[9] * vec.y + mat.m[10] * vec.z + mat.m[11] * vec.w;
-        result.w =  mat.m[12] * vec.x + mat.m[13] * vec.y + mat.m[14] * vec.z + mat.m[15] * vec.w;
+        result.x = mat.m[0] * vec.x + mat.m[1] * vec.y + mat.m[2] * vec.z + mat.m[3] * vec.w;
+        result.y = mat.m[4] * vec.x + mat.m[5] * vec.y + mat.m[6] * vec.z + mat.m[7] * vec.w;
+        result.z = mat.m[8] * vec.x + mat.m[9] * vec.y + mat.m[10] * vec.z + mat.m[11] * vec.w;
+        result.w = mat.m[12] * vec.x + mat.m[13] * vec.y + mat.m[14] * vec.z + mat.m[15] * vec.w;
 
         return result;
     }
 
-    static Mat4 mat4_translation(float tx, float ty, float tz)
-    {
+    static Mat4 mat4_translation(float tx, float ty, float tz) {
         Mat4 mat = identity();
 
         mat.m[3] = tx;
         mat.m[7] = ty;
         mat.m[11] = tz;
+
+        return mat;
+    }
+
+    static Mat4 mat4_rotation_y(float angle) {
+        Mat4 mat = identity();
+
+        mat.m[0] = cos(angle);
+        mat.m[2] = sin(angle);
+        mat.m[8] = -sin(angle);
+        mat.m[10] = cos(angle);
+
+        return mat;
+    }
+
+    static Mat4 mat4_rotation_x(float angle) {
+        Mat4 mat = identity();
+
+        mat.m[5] = cos(angle);
+        mat.m[6] = -sin(angle);
+        mat.m[9] = sin(angle);
+        mat.m[10] = cos(angle);
 
         return mat;
     }
